185a93fa902d84559567bfd6d71984db14c1d392
Sushant R. Dangal <srdarkseer@gmail.com>
2023-10-03 13:42:39 +0545

    hotfix

--- src/views/Home/Hero.tsx
+++ src/views/Home/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <div>
-      <div className="container w-full px-16 py-28">
+      <div className="container w-full py-16 px-28 mt-20">
         <div className="flex flex-col gap-7">
           <div className="w-full flex justify-center">
             <h1 className="text-6xl font-medium leading-[70px] text-center w-10/12 text-head">
@@ -11,7 +11,7 @@ function Hero() {
             </h1>
           </div>
           <div className="w-full flex justify-center">
-            <p className="text-xl text-center text-subHead w-[950px]">
+            <p className="text-xl text-center text-subHead w-[800px]">
               Results That Inspire Trust: Structura Design Co.’s Commitment to
               Quality Projects and Impressive Housing for Customers’s
               Confidence.
