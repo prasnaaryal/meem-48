784c87328bbcd6d657a876af80c44c0d5a7f516f
Sushant R. Dangal <srdarkseer@gmail.com>
2023-10-03 22:56:44 +0545

    mobile responsive tailored

--- src/views/Home/Tailored.tsx
+++ src/views/Home/Tailored.tsx
@@ -3,21 +3,21 @@ import StyledHeader from "@/components/StyledHeader";
 function Tailored() {
   return (
     <div className="container py-36">
-      <div className="grid grid-cols-12 gap-10">
-        <div className="col-span-5 h-[700px]">
+      <div className="grid grid-cols-12 gap-8 sm:gap-10">
+        <div className="col-span-12 sm:col-span-5 w-[360px] sm:w-full h-[500px] sm:h-[700px]">
           <img
             src="/assets/images/tailored.png"
             alt="tailored"
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="col-span-7 flex flex-col justify-center gap-8">
+        <div className="col-span-12 sm:col-span-7 flex flex-col sm:justify-center gap-8">
           <div>
-            <h1 className="mb-6 text-2xl  sm:text-4xl font-medium text-head w-auto">
+            <h1 className="mb-6 text-2xl  sm:text-4xl font-medium text-head">
               Tailored to you
             </h1>
 
-            <p className="text-subHead text-lg font-light leading-8 text-justify">
+            <p className="text-subHead text-lg font-light leading-8 text-justify w-[360px] sm:w-auto">
               Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
               cupidatat non proident, sunt in culpa qui officia.
@@ -45,7 +45,7 @@ function Tailored() {
               </div>
             </div>
 
-            <div className="flex justify-end">
+            <div className="flex sm:justify-end">
               <div className="bg-cream px-8 py-10 w-[395px]">
                 <div className="grid grid-cols-12">
                   <div className="col-span-2">
