52113ea8ec904a2f21b363ffa38124a46dc9b1b4
Sushant R. Dangal <srdarkseer@gmail.com>
2023-10-04 20:48:21 +0545

    updated components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,32 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        white: "bg-white text-indigo hover:bg-white/80",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-lightBlue hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        blue: "bg-indigo text-white hover:bg-indigo/80 ",
-        clean:
-          "bg-transparent text-violet justify-start p-0  underline-on-hover",
-        link: "bg-transparent justify-start p-0  underline-on-hover",
-        service: "bg-white font-semibold text-head hover:bg-white/50",
-        serviceLarge:
-          "bg-lightBlue hover:bg-lightBlue/70 border-r-3 border-indigo",
-        royalBlue: "bg-blue text-white hover:bg-blue/70",
+        default: "bg-primary text-white hover:bg-primary/90",
       },
       size: {
-        default: "h-7 w-44 rounded-none px-5 py-7",
-        sm: "h-9 px-3 rounded-md",
-        md: "h-20 w-48 rounded-none px-5 py-7",
-        lg: "w-52 h-20 rounded-none",
-        xl: "w-96 h-20 rounded-none font-semibold text-xl justify-start gap-3 ",
-        xxl: "w-[350px] xl:w-[463px] h-64 rounded-none font-semibold text-xl justify-start gap-3 ",
-        icon: "h-10 w-10",
+        default: "h-14 w-40 rounded-none px-5 py-7",
+        sm: "rounded-none px-5 py-3",
       },
     },
     defaultVariants: {
